9a5aec94f96363eddb361084c7a0a0d1384ec1e4
aaokunade <67062110+aaokunade@users.noreply.github.com>
2021-01-19 23:03:20 +0000

    Javascript Exercises
    Exercises from the Javascript core2 week 2

--- exercises/A-list-of-names/script.js
+++ exercises/A-list-of-names/script.js
@@ -1,6 +1,23 @@
 function listOfNames(arrayOfPeople) {
   let content = document.querySelector("#content");
+//   for (let i = 0; i < arrayOfPeople.length; i++) {
+//     let headerOne = document.createElement("h1");
+//     headerOne.innerText = arrayOfPeople[i]["name"];
+//     content.appendChild(headerOne);
+//     let headerTwo = document.createElement("h2");
+//     headerTwo.innerText = `${arrayOfPeople[i]["name"]}'s job is ${arrayOfPeople[i]["job"]}`;
+//     content.appendChild(headerTwo);
+//   }
+      arrayOfPeople.forEach(function(obj){
+        let headerOne = document.createElement("h1");
+        headerOne.innerText = obj["name"];
+        content.appendChild(headerOne);
+        let headerTwo = document.createElement("h2");
+        headerTwo.innerText = `${obj["name"]} is a ${obj["job"]}`;
+        content.appendChild(headerTwo);
+      })
 }
+
 
 let people = [
   { name: "Chris", job: "Teacher" },
--- exercises/B-shopping-list/script.js
+++ exercises/B-shopping-list/script.js
@@ -1,5 +1,13 @@
-function shoppingList(arrayOfPeople) {
+function shoppingList(arrayOfItems) {
   // Write your code here...
+  let content = document.querySelector("#content");
+  let newUnOderdList = document.createElement("ul");
+  content.appendChild(newUnOderdList);
+  arrayOfItems.forEach(function (str) {
+    let newList = document.createElement("li");
+    newList.innerText = str;
+    newUnOderdList.appendChild(newList);
+  });
 }
 
 let shopping = ["Milk", "Bread", "Eggs", "A Dinosaur", "Cake", "Sugar", "Tea"];
